c53b3dc5e4ed070118d54254a00b1223584f2527
KrishithaS <7003354@spsd.sk.ca>
2025-11-19 09:54:20 -0600

    added start page

--- sketch.js
+++ sketch.js
@@ -5,11 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let startPage;
+
+function preload(){
+  startPage = loadImage("wordscape_start.png");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(900, 900);
 }
 
 function draw() {
-  background(220);
+  // background(220);
+  image(startPage, 0, 0, width, height);
 }
