4a726e4efd4cb2e5705a876309df37f96f61dfac
KrishithaS <7003354@spsd.sk.ca>
2025-11-20 09:35:35 -0600

    added the next image

--- sketch.js
+++ sketch.js
@@ -6,9 +6,11 @@
 // - describe what you did to take this project "above and beyond"
 
 let startPage;
+let homePage;
 
 function preload(){
   startPage = loadImage("wordscape_start.png");
+  homePage = loadImage("home_page.jpg");
 }
 
 function setup() {
@@ -17,5 +19,24 @@ function setup() {
 
 function draw() {
   // background(220);
-  image(startPage, 0, 0, width, height);
+}
+
+function mousePressed(){
+  if(mouseX <= width && mouseX >= 0 && mouseY >= 0 && mouseY <= height){
+    switchPage();
+  }
+}
+
+function keyPressed(){
+  if(key === " "){
+    image(startPage, 0, 0, width, height);
+  }
+}
+
+function switchPage(){
+  background("white");
+  image(homePage, 150, 0, 600, height);
+  fill("orange");
+  circle(width/2, height/2, 300);
+  noStroke();
 }
